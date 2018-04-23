65a4893b9251c62cb6860114bd15beb836c5fb37
Matthew Castillo <chgreen08@gmail.com>
2018-04-23 06:14:30 -0700

    alternative

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const pg = require("./config/keys");
+const pg = require("./config/herokuKey");
 
 const PORT = process.env.PORT || 5000;
 
